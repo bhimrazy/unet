8e7229c40c43d893760b313f6f780c7d2a6c03ef
Bhimraj Yadav <bhimrajyadav977@gmail.com>
2023-04-11 16:07:04 +0000

    ♻️ ref: Updated train with saving model

--- train.py
+++ train.py
@@ -4,20 +4,20 @@ This module contains the training code for the segmentation task.
 
 (c) 2023 Bhimraj Yadav. All rights reserved.
 """
-import warnings
+from src.dataset import train_dataloader, test_dataloader
+from src.model import UNet
 from tqdm.auto import tqdm
+from datetime import datetime
 from typing import Dict, List, Tuple
 
 import torch
 import torchvision
-from src.config import DEVICE, LEARNING_RATE, NUM_EPOCHS
+from src.config import DEVICE, LEARNING_RATE, NUM_EPOCHS, MODEL_PATH, CHANNELS, OUT_CHANNELS
 
 torchvision.disable_beta_transforms_warning()
 
-from src.model import UNet
-from src.dataset import train_dataloader, test_dataloader
 
-model = UNet(channels=[1, 64, 128, 256, 512], out_channels=1)
+model = UNet(channels=CHANNELS, out_channels=OUT_CHANNELS)
 model.to(DEVICE)
 model = torch.compile(model)
 
@@ -45,9 +45,9 @@ def accuracy(outputs, targets):
     return torch.mean((outputs == targets).float())
 
 
-def train_step(model: torch.nn.Module, 
-               dataloader: torch.utils.data.DataLoader, 
-               loss_fn: torch.nn.Module, 
+def train_step(model: torch.nn.Module,
+               dataloader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
                optimizer: torch.optim.Optimizer) -> Tuple[float, float]:
     """Trains a PyTorch model for a single epoch.
 
@@ -103,131 +103,132 @@ def train_step(model: torch.nn.Module,
     return train_loss, train_acc
 
 
-def test_step(model: torch.nn.Module, 
-              dataloader: torch.utils.data.DataLoader, 
+def test_step(model: torch.nn.Module,
+              dataloader: torch.utils.data.DataLoader,
               loss_fn: torch.nn.Module) -> Tuple[float, float]:
-  """Tests a PyTorch model for a single epoch.
+    """Tests a PyTorch model for a single epoch.
 
-  Turns a target PyTorch model to "eval" mode and then performs
-  a forward pass on a testing dataset.
+    Turns a target PyTorch model to "eval" mode and then performs
+    a forward pass on a testing dataset.
 
-  Args:
-    model: A PyTorch model to be tested.
-    dataloader: A DataLoader instance for the model to be tested on.
-    loss_fn: A PyTorch loss function to calculate loss on the test data.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+    Args:
+      model: A PyTorch model to be tested.
+      dataloader: A DataLoader instance for the model to be tested on.
+      loss_fn: A PyTorch loss function to calculate loss on the test data.
+      device: A target device to compute on (e.g. "cuda" or "cpu").
 
-  Returns:
-    A tuple of testing loss and testing accuracy metrics.
-    In the form (test_loss, test_accuracy). For example:
+    Returns:
+      A tuple of testing loss and testing accuracy metrics.
+      In the form (test_loss, test_accuracy). For example:
 
-    (0.0223, 0.8985)
-  """
-  # set model in eval mode
-  model.eval() 
+      (0.0223, 0.8985)
+    """
+    # set model in eval mode
+    model.eval()
 
-  # Setup test loss and test accuracy values
-  test_loss, test_acc = 0, 0
+    # Setup test loss and test accuracy values
+    test_loss, test_acc = 0, 0
 
-  # Turn on inference context manager
-  with torch.inference_mode():
-      # Loop through DataLoader batches
-      for batch, (X, y) in enumerate(dataloader):
-          # Send data to target device
-          X, y = X.to(DEVICE), y.to(DEVICE)
+    # Turn on inference context manager
+    with torch.inference_mode():
+        # Loop through DataLoader batches
+        for batch, (X, y) in enumerate(dataloader):
+            # Send data to target device
+            X, y = X.to(DEVICE), y.to(DEVICE)
 
-          # 1. Forward pass
-          test_pred_logits = model(X)
+            # 1. Forward pass
+            test_pred_logits = model(X)
 
-          # 2. Calculate and accumulate loss
-          loss = loss_fn(test_pred_logits, y)
-          test_loss += loss.item()
+            # 2. Calculate and accumulate loss
+            loss = loss_fn(test_pred_logits, y)
+            test_loss += loss.item()
 
-          # Calculate and accumulate accuracy
-          test_acc += accuracy(test_pred_logits, y).item()
-          
-  # Adjust metrics to get average loss and accuracy per batch 
-  test_loss = test_loss / len(dataloader)
-  test_acc = test_acc / len(dataloader)
-  return test_loss, test_acc
+            # Calculate and accumulate accuracy
+            test_acc += accuracy(test_pred_logits, y).item()
 
-def train(model: torch.nn.Module, 
-          train_dataloader: torch.utils.data.DataLoader, 
-          test_dataloader: torch.utils.data.DataLoader, 
+    # Adjust metrics to get average loss and accuracy per batch
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+def train(model: torch.nn.Module,
+          train_dataloader: torch.utils.data.DataLoader,
+          test_dataloader: torch.utils.data.DataLoader,
           optimizer: torch.optim.Optimizer,
           loss_fn: torch.nn.Module,
           epochs: int) -> Dict[str, List]:
-  """Trains and tests a PyTorch model.
+    """Trains and tests a PyTorch model.
 
-  Passes a target PyTorch models through train_step() and test_step()
-  functions for a number of epochs, training and testing the model
-  in the same epoch loop.
+    Passes a target PyTorch models through train_step() and test_step()
+    functions for a number of epochs, training and testing the model
+    in the same epoch loop.
 
-  Calculates, prints and stores evaluation metrics throughout.
+    Calculates, prints and stores evaluation metrics throughout.
 
-  Args:
-    model: A PyTorch model to be trained and tested.
-    train_dataloader: A DataLoader instance for the model to be trained on.
-    test_dataloader: A DataLoader instance for the model to be tested on.
-    optimizer: A PyTorch optimizer to help minimize the loss function.
-    loss_fn: A PyTorch loss function to calculate loss on both datasets.
-    epochs: An integer indicating how many epochs to train for.
-    device: A target device to compute on (e.g. "cuda" or "cpu").
+    Args:
+      model: A PyTorch model to be trained and tested.
+      train_dataloader: A DataLoader instance for the model to be trained on.
+      test_dataloader: A DataLoader instance for the model to be tested on.
+      optimizer: A PyTorch optimizer to help minimize the loss function.
+      loss_fn: A PyTorch loss function to calculate loss on both datasets.
+      epochs: An integer indicating how many epochs to train for.
+      device: A target device to compute on (e.g. "cuda" or "cpu").
 
-  Returns:
-    A dictionary of training and testing loss as well as training and
-    testing accuracy metrics. Each metric has a value in a list for 
-    each epoch.
-    In the form: {train_loss: [...],
-                  train_acc: [...],
-                  test_loss: [...],
-                  test_acc: [...]} 
-    For example if training for epochs=2: 
-                 {train_loss: [2.0616, 1.0537],
-                  train_acc: [0.3945, 0.3945],
-                  test_loss: [1.2641, 1.5706],
-                  test_acc: [0.3400, 0.2973]} 
-  """
-  # Create empty results dictionary
-  results = {"train_loss": [],
-      "train_acc": [],
-      "test_loss": [],
-      "test_acc": []
-  }
+    Returns:
+      A dictionary of training and testing loss as well as training and
+      testing accuracy metrics. Each metric has a value in a list for 
+      each epoch.
+      In the form: {train_loss: [...],
+                    train_acc: [...],
+                    test_loss: [...],
+                    test_acc: [...]} 
+      For example if training for epochs=2: 
+                   {train_loss: [2.0616, 1.0537],
+                    train_acc: [0.3945, 0.3945],
+                    test_loss: [1.2641, 1.5706],
+                    test_acc: [0.3400, 0.2973]} 
+    """
+    # Create empty results dictionary
+    results = {"train_loss": [],
+               "train_acc": [],
+               "test_loss": [],
+               "test_acc": []
+               }
 
-  # Loop through training and testing steps for a number of epochs
-  for epoch in tqdm(range(epochs)):
-      train_loss, train_acc = train_step(model=model,
-                                          dataloader=train_dataloader,
-                                          loss_fn=loss_fn,
-                                          optimizer=optimizer)
-      test_loss, test_acc = test_step(model=model,
-          dataloader=test_dataloader,
-          loss_fn=loss_fn)
+    # Loop through training and testing steps for a number of epochs
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model=model,
+                                           dataloader=train_dataloader,
+                                           loss_fn=loss_fn,
+                                           optimizer=optimizer)
+        test_loss, test_acc = test_step(model=model,
+                                        dataloader=test_dataloader,
+                                        loss_fn=loss_fn)
 
-      # Print out what's happening
-      print(
-          f"Epoch: {epoch+1} | "
-          f"train_loss: {train_loss:.4f} | "
-          f"train_acc: {train_acc:.4f} | "
-          f"test_loss: {test_loss:.4f} | "
-          f"test_acc: {test_acc:.4f}"
-      )
+        # Print out what's happening
+        print(
+            f"Epoch: {epoch+1} | "
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
 
-      # Update results dictionary
-      results["train_loss"].append(train_loss)
-      results["train_acc"].append(train_acc)
-      results["test_loss"].append(test_loss)
-      results["test_acc"].append(test_acc)
+        # Update results dictionary
+        results["train_loss"].append(train_loss)
+        results["train_acc"].append(train_acc)
+        results["test_loss"].append(test_loss)
+        results["test_acc"].append(test_acc)
 
-      torch.save(model, f"models/model_chk_{epoch}.pth")
+    # Return the filled results at the end of the epochs
+    torch.save(model.state_dict(), MODEL_PATH)
+    return results
 
-  # Return the filled results at the end of the epochs
-  torch.save(model, "models/f_model.pth")
-  return results
 
-if __name__=="__main__":
+if __name__ == "__main__":
     loss_fn = torch.nn.BCEWithLogitsLoss()
     optimizer = torch.optim.Adam(model.parameters(),
-                             lr=LEARNING_RATE)
-    train(model=model,train_dataloader=train_dataloader,test_dataloader=test_dataloader,optimizer=optimizer,loss_fn=loss_fn,epochs=NUM_EPOCHS)
+                                 lr=LEARNING_RATE)
+    train(model=model, train_dataloader=train_dataloader, test_dataloader=test_dataloader,
+          optimizer=optimizer, loss_fn=loss_fn, epochs=NUM_EPOCHS)
